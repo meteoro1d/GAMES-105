39ca8b75a715ba9421b02871f098bcc20298a52e
meteoro1d <rk7407915@gmail.com>
2024-12-10 20:58:27 +0800

    save

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from lab1.task2_inverse_kinematics import MetaData
+import torch
 
-def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, target_pose):
+def part1_inverse_kinematics(meta_data: MetaData, joint_positions, joint_orientations, target_pose):
     """
     完成函数，计算逆运动学
     输入: 
@@ -14,8 +16,38 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
         joint_positions: 计算得到的关节位置，是一个numpy数组，shape为(M, 3)，M为关节数
         joint_orientations: 计算得到的关节朝向，是一个numpy数组，shape为(M, 4)，M为关节数
     """
-    
-    
+    path, path_name, path1, path2 = meta_data.get_path_from_root_to_end()
+
+    # joint offset
+    path_joint_offset = torch.zeros((len(path), 3))
+
+    print(meta_data.root_joint)
+
+    for i in range (len(path)):
+        if i == 0:
+            parent_position = meta_data.joint_initial_position[path[0]]
+        else:
+            parent_position = meta_data.joint_initial_position[path[i - 1]]
+
+        path_joint_offset[i] = torch.from_numpy(meta_data.joint_initial_position[path[i]] - parent_position)
+
+    print(path_joint_offset)
+
+    #
+    theta = torch.zeros((len(path), 3), requires_grad=True)
+
+    iteration = 10
+
+    for it in range(iteration):
+        last_joint_orientation = None
+        last_joint_position = None
+
+        for i in range(len(path)):
+            if i == 0:
+                last_joint_position = meta_data.joint_initial_position[path[0]]
+                last_joint_orientation = R.from_euler('XYZ', theta[i].numpy(), degrees=True)
+
+
     return joint_positions, joint_orientations
 
 def part2_inverse_kinematics(meta_data, joint_positions, joint_orientations, relative_x, relative_z, target_height):
